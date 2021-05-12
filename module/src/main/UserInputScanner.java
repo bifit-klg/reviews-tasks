2e499b0dd20fa9e332b9098b1ded35b1753734b8
Sergey Platoshin <sergey.platoshin@informicus.ru>
2021-05-13 01:07:56 +0300

    refactorings

--- module/src/main/UserInputScanner.java
+++ module/src/main/UserInputScanner.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
 class UserInputScanner {
-    private Scanner scanner = new Scanner(System.in);
-
     long getUserNumber() {
+        var scanner = new Scanner(System.in);
         System.out.println("Enter your number:");
 
         return scanner.nextLong();
     }
 
     String[] getUserWords() {
+        var scanner = new Scanner(System.in);
         System.out.println("Enter your line:");
         var userLine = scanner.nextLine();
         var userWordsFromLine = userLine.split(" ");
@@ -21,6 +21,7 @@ class UserInputScanner {
     }
 
     String getUserLine() {
+        var scanner = new Scanner(System.in);
         System.out.println("Enter your line:");
 
         return scanner.nextLine();
